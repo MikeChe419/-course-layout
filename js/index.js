039b4b85349f1a865c885f2b328c24c00142e784
Mike <amenhottep@gmail.com>
2022-01-31 13:04:16 +0300

    add.changes

--- js/index.js
+++ js/index.js
@@ -29,15 +29,15 @@ window.addEventListener('DOMContentLoaded', function() {
 });
 
 const swiper = new Swiper('.swiper1', {
-    slidesPerView: 1,
-    allowTouchMove: false,
-    loop: true,
-    effect: 'fade',
-    speed: 10000,
-    autoplay: {
-      delay: 10000
-      },
-    });
+  slidesPerView: 1,
+  allowTouchMove: false,
+  loop: true,
+  effect: 'fade',
+  speed: 10000,
+  autoplay: {
+    delay: 10000
+    },
+  });
 
 const slider2 = new Swiper ('.swiper2', {
       slidesPerView: 1,
@@ -101,7 +101,7 @@ const slider2 = new Swiper ('.swiper2', {
   }
 });
 
-const slider4 = new Swiper ('.swiper4', {
+  const slider4 = new Swiper ('.swiper4', {
       slidesPerView: 1,
       spaceBetween: 50,
       loop: true,
@@ -117,85 +117,37 @@ const slider4 = new Swiper ('.swiper4', {
         nextEl: '.swiper-button-next2',
         prevEl: '.swiper-button-prev2',
         },
-    });
-
-    const slider5 = new Swiper ('.swiper5', {
-      slidesPerView: 1,
-      spaceBetween: 50,
-      pagination: {
-        clickable: true,
-        el: '.events-pagination',
-      },
-      breakpoints: {
-        576: {
-          slidesPerView: 2,
-        },
-        1024: {
-          slidesPerView: 3,
-          navigation: {
-            nextEl: '.events-btn-next',
-            prevEl: '.events-btn-prev',
-            },
-        },
-      },
-    });
-
-const params = {
-    btnClassName: "js-header-dropdown-btn",
-    dropClassName: "js-header-drop",
-    activeClassName: "is-active-2",
-    disabledClassName: "is-disabled"
-  }
-
-  function onDisable(evt) {
-    if (evt.target.classList.contains(params.disabledClassName)) {
-      evt.target.classList.remove(params.disabledClassName, params.activeClassName);
-      evt.target.removeEventListener("animationend", onDisable);
-    }
-  }
-
-
-function setMenuListener() {
-  document.addEventListener("click", (evt) => {
-    const activeElements = document.querySelectorAll(`.${params.btnClassName}.${params.activeClassName}, .${params.dropClassName}.${params.activeClassName}`);
-
-    if (activeElements.length && !evt.target.closest(`.${params.activeClassName}`))  {
-      activeElements.forEach((current) => {
-        if(current.classList.contains(params.btnClassName)) {
-          current.classList.remove(params.activeClassName);
-        } else {
-          current.classList.add(params.disabledClassName);
-        }
-      });
-    }
-
-    if (evt.target.closest('.js-header-dropdown-btn')) {
-      const btn = evt.target.closest('.js-header-dropdown-btn');
-      const genres = btn.dataset.genres;
-      const drop = document.querySelector(`.${params.dropClassName}[data-target="${genres}"]`);
-
-      btn.classList.toggle('is-active-2');
-
-      if (!drop.classList.contains('is-active-2')) {
-        drop.classList.add('is-active-2');
-        drop.addEventListener("animationend", onDisable);
-      } else {
-        drop.classList.add('is-disabled');
-      }
-    }
   });
-}
 
-const ArtDefault = () => {
+  const slider5 = new Swiper ('.swiper5', {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    pagination: {
+      clickable: true,
+      el: '.events-pagination',
+    },
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+        navigation: {
+          nextEl: '.events-btn-next',
+          prevEl: '.events-btn-prev',
+        },
+      },
+    },
+  });
+
+function ArtDefault() {
   const element = document.querySelector('.art');
   const choices = new Choices(element, {
     searchEnabled: false,
-});
+  });
 }
 
 ArtDefault();
-
-$(".accordion").accordion();
 
 document.querySelectorAll('.swpr-slde').forEach(function(tabsBtn) {
   tabsBtn.addEventListener('click', function(e){
@@ -209,6 +161,14 @@ btn.classList.remove('swpr-slde-active')});
 
 document.querySelector(`[data-target = ${picture}]`).classList.add('picture-description-container-active');
   });
+});
+
+$(".accordion").accordion();
+
+window.addEventListener('DOMContentLoaded', function() {
+  document. querySelector('.clother-btn').addEventListener('click', function() {
+    document.querySelector('.picture-description-container').classList.remove('picture-description-container-active')
+  })
 });
 
 document.querySelectorAll('.catalog-title-btn').forEach(function(tabsBtn) {
@@ -295,3 +255,48 @@ function init(){
 tippy('.js-tooltip', {
   theme: 'violet',
 });
+
+const params = {
+  btnClassName: "js-header-dropdown-btn",
+  dropClassName: "js-header-drop",
+  activeClassName: "is-active-2",
+  disabledClassName: "is-disabled"
+}
+
+    function onDisable(evt) {
+      if (evt.target.classList.contains(params.disabledClassName)) {
+        evt.target.classList.remove(params.disabledClassName, params.activeClassName);
+        evt.target.removeEventListener("animationend", onDisable);
+      }
+    }
+
+    function setMenuListener() {
+    document.addEventListener("click", (evt) => {
+      const activeElements = document.querySelectorAll(`.${params.btnClassName}.${params.activeClassName}, .${params.dropClassName}.${params.activeClassName}`);
+
+      if (activeElements.length && !evt.target.closest(`.${params.activeClassName}`))  {
+        activeElements.forEach((current) => {
+          if(current.classList.contains(params.btnClassName)) {
+            current.classList.remove(params.activeClassName);
+          } else {
+            current.classList.add(params.disabledClassName);
+          }
+        });
+      }
+
+      if (evt.target.closest('.js-header-dropdown-btn')) {
+        const btn = evt.target.closest('.js-header-dropdown-btn');
+        const genres = btn.dataset.genres;
+        const drop = document.querySelector(`.${params.dropClassName}[data-target="${genres}"]`);
+
+        btn.classList.toggle('is-active-2');
+
+        if (!drop.classList.contains('is-active-2')) {
+          drop.classList.add('is-active-2');
+          drop.addEventListener("animationend", onDisable);
+        } else {
+          drop.classList.add('is-disabled');
+        }
+      }
+    });
+  };
